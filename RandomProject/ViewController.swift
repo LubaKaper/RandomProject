5e2496bc996bd9c8220ea9cc31e44e76c39f36af
LubaKaper <liubovkaper@pursuit.com>
2020-03-12 17:24:19 -0400

    Initial Commit

---
+++ RandomProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomProject
+//
+//  Created by Liubov Kaper  on 3/12/20.
+//  Copyright © 2020 Luba Kaper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
