0fe34891286dd9fdd57bc9b433dc02f528697d07
LubaKaper <liubovkaper@pursuit.com>
2020-03-12 17:27:53 -0400

    testing push

--- RandomProject/AppDelegate.swift
+++ RandomProject/AppDelegate.swift
@@ -11,6 +11,7 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    // no no no
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
